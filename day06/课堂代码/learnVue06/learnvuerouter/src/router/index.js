3604524f00a72e7f6b4b2eea772255c0997d5660
XuRiven <XuRiven_Jay@163.com>
2020-01-20 16:02:46 +0800

    导航守卫

--- day06/课堂代码/learnVue06/learnvuerouter/src/router/index.js
+++ day06/课堂代码/learnVue06/learnvuerouter/src/router/index.js
@@ -29,33 +29,45 @@ const routes = [
   {
     path: '/home',
     component: Home,
-    children:[
-      {
-        path:'',
+    // 定义标题
+    meta: {
+      title: '首页'
+    },
+    children: [{
+        path: '',
         redirect: 'homeNews'
       },
       {
         // 路由嵌套里的路径不需要加/
-        path:'homeNews',
-        component:HomeNews
+        path: 'homeNews',
+        component: HomeNews
       },
       {
-        path:'homeMessage',
-        component:HomeMessage
+        path: 'homeMessage',
+        component: HomeMessage
       }
     ]
   },
   {
     path: '/about',
-    component: About
+    component: About,
+    meta: {
+      title: '关于'
+    }
   },
   {
     path: '/user/:userId',
-    component: User
+    component: User,
+    meta: {
+      title: '个人中心'
+    }
   },
   {
-    path:'/profile',
-    component:Profile
+    path: '/profile',
+    component: Profile,
+    meta: {
+      title: '档案'
+    }
   }
 ]
 
@@ -74,5 +86,29 @@ const router = new VueRouter({
   linkActiveClass: 'active'
 })
 
+
+/* 
+我们可以利用beforeEach来完成标题的修改.
+首先, 我们可以在钩子当中定义一些标题, 可以利用meta来定义
+其次, 利用导航守卫,修改我们的标题.
+
+导航钩子的三个参数解析:
+to: 即将要进入的目标的路由对象.
+from: 当前导航即将要离开的路由对象.
+next: 调用该方法后, 才能进入下一个钩子.
+*/
+// 前置守卫(guard)
+router.beforeEach((to, from, next) => {
+  // 从from跳转到to
+  document.title = to.matched[0].meta.title
+  console.log('++++');
+  next()
+})
+
+// 后置钩子(hook)
+router.afterEach( route => {
+  console.log('----');
+  
+})
 // 3.将router对象传入到Vue实例
 export default router
