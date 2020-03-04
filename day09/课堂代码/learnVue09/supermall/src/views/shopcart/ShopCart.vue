810284f8d926d5e14aabcdc01cb3b41a0697d89f
XuRiven <XuRiven_Jay@163.com>
2020-03-04 16:33:11 +0800

    商城-商品购物车列表展示

--- day09/课堂代码/learnVue09/supermall/src/views/shopcart/ShopCart.vue
+++ day09/课堂代码/learnVue09/supermall/src/views/shopcart/ShopCart.vue
@@ -1,13 +1,51 @@
 <template>
-<h2>购物车</h2>
+  <div class="cart">
+    <!-- 导航   -->
+    <nav-bar class="nav-bar">
+      <div slot="center">购物车({{cartLength}})</div>
+    </nav-bar>
+    <!-- 商品列表   -->
+    <cart-list/>
+
+    <!-- 底部汇总   -->
+    <cart-bottom-bar/>
+  </div>
 </template>
 
 <script>
-export default {
-  name:'ShopCart'
-}
+  //从vuex中导入mapGetters
+  import {mapGetters} from 'vuex'
+  import NavBar from "components/common/navbar/NavBar";
+  import CartList from "./childComps/CartList";
+  import CartBottomBar from "./childComps/CartBottomBar";
+
+  export default {
+    name: 'ShopCart',
+    components: {
+      CartList,
+      NavBar,
+      CartBottomBar
+    },
+    computed: {
+      //mapGetters 辅助函数仅仅是将 store 中的 getter 映射到局部计算属性
+      ...mapGetters(['cartLength'])
+
+      //如果你想将一个 getter 属性另取一个名字，使用对象形式：
+      // ...mapGetters({
+      //   length:'cartLength',
+      //   list:'cartList'
+      // })
+    }
+  }
 </script>
 
 <style>
-
+  .cart{
+    height: 100vh;
+  }
+  .nav-bar {
+    background-color: var(--color-tint);
+    color: #fff;
+    font-weight: 700;
+  }
 </style>
---
+++ day09/课堂代码/learnVue09/supermall/src/views/shopcart/childComps/CartBottomBar.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="bottom-menu">
+    <CheckButton class="select-all"/>
+    <span>全选</span>
+    <span class="total-price">合计: ¥{{totalPrice}}</span>
+    <span class="buy-product">去计算({{cartLength}})</span>
+  </div>
+</template>
+
+<script>
+  import CheckButton from "./CheckButton";
+
+  export default {
+    name: "CartBottomBar",
+    components: {
+      CheckButton
+    },
+    computed: {
+      totalPrice() {
+        return this.$store.state.cartList.filter(item => {
+          return item.checked
+        }).reduce((previousValue, item) => {
+          return previousValue + item.price * item.count
+        }, 0).toFixed(2)
+      },
+      cartLength(){
+        return this.$store.state.cartList.filter(item => item.checked).length
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .bottom-menu {
+    width: 100%;
+    height: 44px;
+    background-color: #eee;
+    position: fixed;
+    bottom: 49px;
+    left: 0;
+    box-shadow: 0 -2px 3px rgba(0, 0, 0, .2);
+    font-size: 14px;
+    color: #888;
+    line-height: 44px;
+    padding-left: 35px;
+    box-sizing: border-box;
+  }
+
+  .bottom-menu .select-all {
+    position: absolute;
+    line-height: 0;
+    left: 12px;
+    top: 13px;
+  }
+
+  .bottom-menu .total-price {
+    margin-left: 15px;
+    font-size: 16px;
+    color: #666;
+  }
+
+  .bottom-menu .buy-product {
+    background-color: orangered;
+    color: #fff;
+    width: 100px;
+    height: 44px;
+    text-align: center;
+    line-height: 44px;
+    float: right;
+  }
+</style>
