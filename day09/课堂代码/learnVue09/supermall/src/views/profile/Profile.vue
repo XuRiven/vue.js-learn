060b8ac1d86d185a8022ccf700599d4567f77d14
XuRiven <XuRiven_Jay@163.com>
2020-03-06 10:15:48 +0800

    商城-profile

--- day09/课堂代码/learnVue09/supermall/src/views/profile/Profile.vue
+++ day09/课堂代码/learnVue09/supermall/src/views/profile/Profile.vue
@@ -1,13 +1,52 @@
 <template>
-<h2>我的</h2>
+  <div id="profile">
+    <nav-bar class="nav-bar">
+      <div slot="center">我的档案</div>
+    </nav-bar>
+    <user-info/>
+    <account-info/>
+    <normal-list-view :listData="orderList"/>
+    <normal-list-view :listData="serviceList"/>
+  </div>
 </template>
 
 <script>
-export default {
-  name:'Profile'
-}
+  import NavBar from "components/common/navbar/NavBar";
+  import UserInfo from "./childComps/UserInfo";
+  import AccountInfo from "./childComps/AccountInfo";
+  import NormalListView from "./childComps/NormalListView";
+
+  export default {
+    name: 'Profile',
+    components: {
+      NavBar,
+      UserInfo,
+      AccountInfo,
+      NormalListView
+    },
+    data(){
+      return{
+        orderList: [
+          {icon: 'message.svg', info: '我的消息'},
+          {icon: 'pointer.svg', info: '积分商城'},
+          {icon: 'vip.svg', info: '会员卡'},
+        ],
+        serviceList: [
+          {icon: 'cart.svg', info: '我的购物车'},
+          {icon: 'shopping.svg', info: '下载购物APP'},
+        ]
+      }
+    }
+  }
 </script>
 
 <style>
+  #profile {
+    background-color: #f2f2f2;
+  }
 
+  .nav-bar {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
